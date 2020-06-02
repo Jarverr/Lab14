c5818b3fb1d693762de3fd065cdc335598546d1d
Michael <krzyzowskim1998@gmail.com>
2020-06-02 20:19:20 +0200

    first commit

---
+++ RegisterCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab14
+{
+    public class RegisterCommand : ICommand
+    {
+        private RegistretionModelValidator _validator = new RegistretionModelValidator();
+        public event EventHandler CanExecuteChanged // przy przypisaniu event hendlera do  canexectueChange przepisujemy ten event hendler to requery suggested
+        {
+            add { CommandManager.RequerySuggested += value; } //
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+
+            var model = parameter as RegistrationModel;
+            if (model is null)
+            {
+                return false;
+            }
+            var result = _validator.Validate(model);
+            model.Errors = string.Join(" ", result.Errors);
+            return result.IsValid;
+                //_validator.Validate(model).IsValid;
+                //            !string.IsNullOrWhiteSpace(model.Email) && !string.IsNullOrWhiteSpace(model.Password) && model.Accept;
+        }
+
+        public void Execute(object parameter)
+        {
+            var model = parameter as RegistrationModel;
+            MessageBox.Show($"Zarejestrowano usera {model.Email}",  "Rejestracja pomyślna", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ RegistretionModelValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    public class RegistretionModelValidator: AbstractValidator<RegistrationModel>
+    {
+        public RegistretionModelValidator()
+        {
+            this.CascadeMode = CascadeMode.StopOnFirstFailure;
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress();
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(6)
+                .Must(r => !(r == r.ToLower() || r == r.ToUpper()))
+                .Matches(@"(.*[!@#$%^&*\-].*)+"); //http://regex101.com/
+            RuleFor(x => x.Accept)
+                .Must(x => x)
+                .WithMessage("Musisz wyrazic zgode na warunki usługi");
+            
+        }
+    }
+}
